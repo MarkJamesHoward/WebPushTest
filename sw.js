1fdbd5b3a97d9ad67f5666b929b8bf2fc71f6bdc
markjameshoward <markjameshoward@hotmail.co.uk>
2019-10-05 14:01:45 +1300

    fix

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-self.addEventListener("push", e => {
+self.addEventListener('push', e => {
   var options = {
-    body: "test",
-    icon: "images/example.png",
+    body: 'test',
+    icon: 'images/example.png',
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
-      primaryKey: "2"
+      primaryKey: '2'
     },
     actions: [
       {
-        action: "explore",
-        title: "Explore this new world",
-        icon: "images/checkmark.png"
+        action: 'explore',
+        title: 'Explore this new world',
+        icon: 'images/checkmark.png'
       },
-      { action: "close", title: "Close", icon: "images/xmark.png" }
+      { action: 'close', title: 'Close', icon: 'images/xmark.png' }
     ]
   };
-  e.waitUntil(self.registration.showNotification("Hello world!", options));
+  e.waitUntil(self.registration.showNotification('Hello world!', options));
 });
